9ee888272659eecf129e4fd3678dd211232d2ff0
Mehdi Bounouif <mbounoui@c1r3p2.1337.ma>
2025-07-14 16:17:58 +0100

    check input file if exists and duplicate fds

--- execution/execute_redirection_command.c
+++ execution/execute_redirection_command.c
@@ -6,11 +6,13 @@
 /*   By: mbounoui <mbounoui@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 14:00:40 by mbounoui          #+#    #+#             */
-/*   Updated: 2025/07/14 14:00:41 by mbounoui         ###   ########.fr       */
+/*   Updated: 2025/07/14 16:16:53 by mbounoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+#include <fcntl.h>
+#include <readline/history.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -34,30 +36,22 @@ int	check_if_exist(t_redirection *node)
 	return (1);
 }
 
-void	execute_redirection_command(t_tree *node, t_env *env, char **envp)
+void	close_fds(int *list)
+{
+	int	i;
+
+	i = 0;
+	while (list[i])
+	{
+		close(list[i]);
+		i++;
+	}
+}
+
+void	dup_fds(t_redirection *node, t_env *env, char **envp)
 {
 	(void)env;
 	(void)envp;
-	//int	fd;
-	//pid_t pid;
-	if (!check_if_exist(node->redirect))
-		return ;
-/*
-	pid = fork();
-	if (pid == 0)
-	{
-		//child	process;
-	}
-	else if (pid > 0)
-	{
-		//parrent process;
-	}
-	else
-	{
-		// error pid = -1;
-	}
-	// 1 check input file if is exist;
-	//
 	// 2 check flags of stdin (input file or heredoc)
 	//		if herdoc
 	//			dup2(last_fd, 0);
@@ -72,7 +66,64 @@ void	execute_redirection_command(t_tree *node, t_env *env, char **envp)
 	//			fd = open("out_file", O_CREAT | O_TRUNK);
 	//		else if (append)
 	//			fd = open("out_file", O_CREAT | O_APPEND);
+	int	in_fd;
+	int	out_fd;
+
+	if (node->hdc)
+	{
+		dup2(node->last_fd, 0);
+		close_fds(node->heredoc_fds);
+	}
+	else if (node->in)
+	{
+		if ((in_fd = open(node->in_file, O_RDONLY)) == -1)
+			exit(-1);
+		dup2(in_fd, 0);
+		close(in_fd);
+		close_fds(node->heredoc_fds);
+	}
+	if (node->out_type == R_APPEND)
+	{
+		if ((out_fd = open(node->out_file, O_CREAT | O_APPEND, 0777)) == -1)
+			exit(-1);
+		dup2(out_fd, 1);
+	}
+	else if (node->out_type == R_OUT)
+	{
+		if ((out_fd = open(node->out_file, O_CREAT | O_TRUNC, 0777)) == -1)
+			exit(-1);
+		dup2(out_fd, 1);
+	}
 }
-*/
+
+
+
+void	execute_redirection_command(t_tree *node, t_env *env, char **envp)
+{
+	(void)env;
+	(void)envp;
+	pid_t pid;
+
+	// 1 check input file if is exist;
+	if (!check_if_exist(node->redirect))
+		return ;
+	pid = fork();
+	if (pid == 0)
+	{
+		// duplicate fds
+		dup_fds(node->redirect, env, envp);
+	//	char *path = find_path(node , env);
+	//	execve(path, node->command->args, envp);
+	//	perror("execve");
+	//	exit(1);
+		//child	process;
+	}
+	else if (pid > 0)
+	{
+		int status;
+		waitpid(pid, &status, 0);
+//		ret = WEXITSTATUS(status);
+		//parrent process;
+	}
 }
 
