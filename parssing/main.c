4ef5047ce3aa8a3211eabdb6774bade8f26a479c
Mehdi Bounouif <mbounoui@c1r5p8.1337.ma>
2025-07-21 17:53:22 +0100

    fork heredoc

--- parssing/main.c
+++ parssing/main.c
@@ -6,11 +6,13 @@
 /*   By: moraouf <moraouf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 08:03:44 by mbounoui          #+#    #+#             */
-/*   Updated: 2025/07/21 11:25:41 by mbounoui         ###   ########.fr       */
+/*   Updated: 2025/07/21 17:23:25 by mbounoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+#include <sys/wait.h>
+#include <unistd.h>
 
 int global(int state)
 {
@@ -20,31 +22,35 @@ int global(int state)
 	return value;
 }
 
+void	open_her(t_mini minishell)
+{
+	pid_t pid;
+	pid = fork();
+	if (pid == 0)
+		open_herdocs(minishell.tree, minishell.env);
+	else
+		waitpid(pid, NULL, 0);
+}
+
 int	main(int c, char **v __attribute__((unused)), char **env)
 {
 	t_mini  minishell;
 
-	// check stdin stderr stdout with isatty();
 	if (c != 1)
 	{
 		ft_putendl_fd("This program does not accept arguments", 2);
 		exit(0);
 	}
 	ft_bzero(&minishell, sizeof(t_mini));
-	// Initialize env only once!
 	get_env(&minishell, env);
 	while (1)
 	{
-		//handle signals
 		handle_signal();
-		// PARSSING
 		if (!readline_and_parssing(&minishell))
 			continue;
-		open_herdocs(minishell.tree, minishell.env);
-		//execute_full_command(minishell.tree, minishell.env, env);
-		print_ast(minishell.tree, 0);
-		//printf("exit status = %d\n", minishell.tree->ret);
-    	// FREE
+		open_her(minishell);
+		execute_full_command(minishell.tree, minishell.env, env);
+		//print_ast(minishell.tree, 0);
 		ft_free(&minishell);
 	}
 	exit(minishell.tree->ret);
